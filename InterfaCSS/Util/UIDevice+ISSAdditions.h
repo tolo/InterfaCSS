4338711061d6f1809dbce57661b44de06625d456
Tobias Löfstrand <tobias@leafnode.se>
2015-09-13 22:52:06 +0200

    Comment update.

--- InterfaCSS/Util/UIDevice+ISSAdditions.h
+++ InterfaCSS/Util/UIDevice+ISSAdditions.h
@@ -1,6 +1,10 @@
 //
-// Created by Tobias Löfstrand on 15-09-11.
-// Copyright (c) 2015 Leafnode AB. All rights reserved.
+//  UIDevice+ISSAdditions.h
+//  Part of InterfaCSS - http://www.github.com/tolo/InterfaCSS
+//
+//  Created by Tobias Löfstrand on 2015-09-11.
+//  Copyright (c) 2014 Leafnode AB.
+//  License: MIT (http://www.github.com/tolo/InterfaCSS/LICENSE)
 //
 
 #import <Foundation/Foundation.h>
